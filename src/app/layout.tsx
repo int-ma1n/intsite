59b00913a9044eab50cb117056a8b09b56076d1c
int_ma1n <119791740+int-ma1n@users.noreply.github.com>
2026-03-19 23:29:35 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Analytics />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
